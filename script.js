58165e75c09cf3f8dd49e3704588af49217a9f31
Augustin Briolon <augustin.briolon@gmail.com>
2024-11-11 17:13:51 +0100

    :rocket: project of my life

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const result = document.getElementById('result');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    result.innerHTML = "Geolocation is not supported by this browser.";
+  }
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const apiKey = '18711f89c687998fa3625165faa8e436'
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const weather = data.weather[0].main.toLowerCase();
+        console.log(weather);
+        if (weather.includes('rain')) {
+          result.innerHTML = "yes";
+        } else {
+          result.innerHTML = "no";
+        }
+      })
+      .catch(() => {
+        result.innerHTML = "Unable to retrieve weather data.";
+      });
+  }
+
+  function error() {
+    result.innerHTML = "Unable to retrieve your location.";
+  }
+});
