6ec3c2ff9017762fe3964d81d09732e0df21e465
Augustin Briolon <augustinbriolon@MacBook-Pro-de-Augustin.local>
2024-11-20 10:05:54 +0100

    :mag: add seo tag and f avicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Search from "@/components/Search";
+import Head from "next/head";
 import { useState, useEffect } from "react";
 
 export default function Home() {
@@ -40,6 +41,26 @@ export default function Home() {
 
   return (
     <main className="container mx-auto h-screen p-2">
+      <Head>
+        <meta property="og:title" content="Is It Raining" />
+        <meta property="og:description" content="Check if it's raining in your place" />
+        <meta property="og:url" content="https://isitraining.vercel.app/" />
+        <meta property="og:image" content="/assets/ogimage.png" />
+
+        <meta name="google-site-verification" content="Ou7Z9Vh9IOICDBGc1TRKhWy-mlGZYcjVMJ0_LeHWOn8" />
+        <meta name="description" content="Check if it's raining in your place" />
+
+        <link rel="canonical" href="https://isitraining.vercel.app/" />
+
+        <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+
+        <title>Is It Raining</title>
+      </Head>
       <h1 className="text-4xl">Is It Raining {cityHome && <span className="text-xl">in {cityHome}</span>}</h1>
       <p>Check if it&apos;s raining at your location</p>
       <div className="h-3/4 w-full flex items-center justify-center">
